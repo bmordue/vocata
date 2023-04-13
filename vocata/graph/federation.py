a70764caa804b37f4f71cf64476b0c6d235395a5
Dominik George <nik@naturalnet.de>
2023-04-13 23:10:41 +0200

    Fix logging and logic in HTTP signing

--- vocata/graph/federation.py
+++ vocata/graph/federation.py
@@ -2,7 +2,9 @@ from base64 import b64encode
 from email.utils import formatdate
 from hashlib import sha256
 from importlib.metadata import metadata
+from pprint import pformat
 from typing import ClassVar, TYPE_CHECKING
+from urllib.parse import urlparse
 
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 from cryptography.hazmat.primitives import serialization as crypto_serialization
@@ -10,6 +12,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from requests import Request, Response, Session
 from requests.auth import AuthBase
+from requests.exceptions import JSONDecodeError
 
 from .authz import HAS_ACTOR, HAS_TRANSIENT_AUDIENCE, HAS_TRANSIENT_INBOXES, PUBLIC_ACTOR
 
@@ -33,8 +36,10 @@ class HTTPSignatureAuth(AuthBase):
             self._key_id, _ = self._graph.get_private_key(actor)
             if not self._key_id:
                 raise ValueError(f"No known key ID for {actor}")
+            self._graph._logger.debug("Found key ID %s for %s", self._key_id, actor)
         else:
             self._key_id = key_id
+        self._graph._logger.debug("Creating HTTP signer with key ID %s", self._key_id)
 
         # FIXME enable once we can distinguish local and remote
         # self._graph.pull(self._key_id)
@@ -49,17 +54,26 @@ class HTTPSignatureAuth(AuthBase):
                 password=None,
                 backend=crypto_default_backend(),
             )
+            self._graph._logger.debug("Private key with ID %s found", self._key_id)
         else:
             self._private_key = None
+            self._graph._logger.debug("Private key with ID %s not found", self._key_id)
 
         if self._public_key_pem:
             self._public_key = crypto_serialization.load_pem_public_key(
                 self._public_key_pem.encode("utf-8"), backend=crypto_default_backend()
             )
+            self._graph._logger.debug("Public key with ID %s not found", self._key_id)
         else:
             self._public_key = None
+            self._graph._logger.warning("Public key with ID %s not found", self._key_id)
 
     def __call__(self, request: Request) -> Request:
+        self._graph._logger.debug(
+            "Signing header for %s request to %s", request.method, request.url
+        )
+        self._graph._logger.debug("Request headers before signing: %s", pformat(request.headers))
+
         if not self._private_key:
             raise ValueError(f"Private key {self._key_id} is unknown")
 
@@ -68,24 +82,34 @@ class HTTPSignatureAuth(AuthBase):
         for header in self._HEADERS:
             if header not in request.headers:
                 if header.lower() == "date":
+                    self._graph._logger.debug("Adding Date header to request")
                     request.headers["Date"] = formatdate(timeval=None, localtime=False, usegmt=True)
                 elif header.lower() == "digest" and request.body is not None:
+                    self._graph._logger.debug("Adding Digest header to request")
                     request.headers["Digest"] = "SHA-256=" + b64encode(
                         sha256(request.body).digest()
                     ).decode("utf-8")
+                elif header.lower() == "host":
+                    self._graph._logger.debug("Adding Host header to request")
+                    request.headers["Host"] = urlparse(request.url).netloc
 
-                if header.lower() == "(request-target)":
-                    method = request.method.lower()
-                    path = request.path_url
-                    signature_data.append(f"(request-target): {method} {path}")
-                    used_headers.append("(request-target)")
-                elif header in request.headers:
-                    name = header.lower()
-                    value = request.headers[header]
-                    signature_data.append(f"{name}: {value}")
-                    used_headers.append(name)
+            if header.lower() == "(request-target)":
+                self._graph._logger.debug("Adding (request-target) pseudo-header to signature")
+                method = request.method.lower()
+                path = request.path_url
+                signature_data.append(f"(request-target): {method} {path}")
+                used_headers.append("(request-target)")
+            elif header in request.headers:
+                self._graph._logger.debug("Adding %s header to signature", header)
+                name = header.lower()
+                value = request.headers[header]
+                signature_data.append(f"{name}: {value}")
+                used_headers.append(name)
+            else:
+                self._graph._logger.debug("Header %s not found, skipping", header)
 
         signature_text = "\n".join(signature_data)
+        self._graph._logger.debug("Signing header: %s", signature_text)
         signature = b64encode(
             self._private_key.sign(
                 signature_text.encode("utf-8"), padding.PKCS1v15(), hashes.SHA256()
@@ -99,8 +123,11 @@ class HTTPSignatureAuth(AuthBase):
             f'signature="{signature}"',
         ]
         signature_header = ",".join(signature_fields)
+        self._graph._logger.debug("Created signature header %s", signature_header)
 
         request.headers["Signature"] = signature_header
+        self._graph._logger.debug("Request headers after signing: %s", pformat(request.headers))
+
         return request
 
 
@@ -138,7 +165,16 @@ class ActivityPubFederationMixin:
             auth = HTTPSignatureAuth(self, actor)
             self._logger.debug("Enabled HTTP signatures for request")
 
-        return self.http_session.request(method, target, headers=headers, json=data, auth=auth)
+        res = self.http_session.request(method, target, headers=headers, json=data, auth=auth)
+        if res.status_code >= 400:
+            try:
+                error = res.json()
+                self._logger.error("Request failed with error: %s", pformat(error))
+            except JSONDecodeError:
+                error = res.text
+                self._logger.error("Request failed with error: %s", error)
+
+        return res
 
     def pull(self, subject: str, actor: str = PUBLIC_ACTOR) -> tuple[bool, Response]:
         # FIXME distinguish between local and remote
