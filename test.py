09fe5cfd720c146a6f8f7a9ece3d8290f745af05
Dominik George <nik@naturalnet.de>
2023-04-13 13:59:23 +0200

    Remove accidentally commited test script

--- test.py
+++
@@ -1,9 +0,0 @@
-from vocata.graph import get_graph
-
-graph = get_graph()
-
-subject = "https://vocatadev.pagekite.me/testcreate-priv"
-actor = "https://vocatadev.pagekite.me/users/tester1"
-target = "https://floss.social/users/Natureshadow/inbox"
-
-res = graph.push_to(target, subject, actor)
