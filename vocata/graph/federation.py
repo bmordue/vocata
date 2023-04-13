2d06e395caef8c70841e8447bbdd4de126a1c3fc
Dominik George <nik@naturalnet.de>
2023-04-13 17:17:32 +0200

    Implement HTTP signatures ourselves

--- vocata/graph/federation.py
+++ vocata/graph/federation.py
@@ -1,9 +1,105 @@
+from base64 import b64encode
+from email.utils import formatdate
+from hashlib import sha256
 from importlib.metadata import metadata
+from typing import ClassVar, TYPE_CHECKING
 
-from requests import Response, Session
-from requests_http_message_signatures import HTTPSignatureHeaderAuth
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from requests import Request, Response, Session
+from requests.auth import AuthBase
+
+if TYPE_CHECKING:
+    from .graph import ActivityPubGraph
 
 CONTENT_TYPE = 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+
+
+class HTTPSignatureAuth(AuthBase):
+    _HEADERS: ClassVar[list[str]] = ["(request-target)", "host", "date", "digest"]
+
+    def __init__(
+        self, graph: "ActivityPubGraph", actor: str | None = None, key_id: str | None = None
+    ):
+        if actor and key_id:
+            raise TypeError("Only one of actor or key_id must be provided.")
+
+        self._graph = graph
+        if actor:
+            self._key_id, _ = self._graph.get_private_key(actor)
+            if not self._key_id:
+                raise ValueError(f"No known key ID for {actor}")
+        else:
+            self._key_id = key_id
+
+        # FIXME enable once we can distinguish local and remote
+        # self._graph.pull(self._key_id)
+
+        self._private_key_pem = self._graph.get_private_key_by_id(self._key_id)
+        self._public_key_pem = self._graph.get_public_key_by_id(self._key_id)
+
+        if self._private_key_pem:
+            self._private_key = crypto_serialization.load_pem_private_key(
+                self._private_key_pem.encode("utf-8"),
+                # FIXME support a configurable password
+                password=None,
+                backend=crypto_default_backend(),
+            )
+        else:
+            self._private_key = None
+
+        if self._public_key_pem:
+            self._public_key = crypto_serialization.load_pem_public_key(
+                self._public_key_pem.encode("utf-8"), backend=crypto_default_backend()
+            )
+        else:
+            self._public_key = None
+
+    def __call__(self, request: Request) -> Request:
+        if not self._private_key:
+            raise ValueError(f"Private key {self._key_id} is unknown")
+
+        signature_data = []
+        used_headers = []
+        for header in self._HEADERS:
+            if header not in request.headers:
+                if header.lower() == "date":
+                    request.headers["Date"] = formatdate(timeval=None, localtime=False, usegmt=True)
+                elif header.lower() == "digest" and request.body is not None:
+                    request.headers["Digest"] = "SHA-256=" + b64encode(
+                        sha256(request.body).digest()
+                    ).decode("utf-8")
+
+                if header.lower() == "(request-target)":
+                    method = request.method.lower()
+                    path = request.path_url
+                    signature_data.append(f"(request-target): {method} {path}")
+                    used_headers.append("(request-target)")
+                elif header in request.headers:
+                    name = header.lower()
+                    value = request.headers[header]
+                    signature_data.append(f"{name}: {value}")
+                    used_headers.append(name)
+
+        signature_text = "\n".join(signature_data)
+        signature = b64encode(
+            self._private_key.sign(
+                signature_text.encode("utf-8"), padding.PKCS1v15(), hashes.SHA256()
+            )
+        ).decode("utf-8")
+        headers = " ".join(used_headers)
+        signature_fields = [
+            f'keyId="{self._key_id}"',
+            'algorithm="rsa-sha256"',
+            f'headers="{headers}"',
+            f'signature="{signature}"',
+        ]
+        signature_header = ",".join(signature_fields)
+
+        request.headers["Signature"] = signature_header
+        return request
 
 
 class ActivityPubFederationMixin:
@@ -34,14 +130,7 @@ class ActivityPubFederationMixin:
         auth = None
         if method == "POST":
             headers["Content-Type"] = CONTENT_TYPE
-
-            priv_id, priv_pem = self.get_private_key(actor)
-            auth = HTTPSignatureHeaderAuth(
-                algorithm="rsa-sha256",
-                key=priv_pem.encode("utf-8"),
-                key_id=priv_id,
-                headers=["(request-target)", "host", "date", "digest"],
-            )
+            auth = HTTPSignatureAuth(self, actor)
 
         return self.http_session.request(method, target, headers=headers, json=data, auth=auth)
 
