8810c87a22687443cb78eae2ff51c982f376306a
Dominik George <nik@naturalnet.de>
2023-04-17 23:34:37 +0200

    Make name in actor creation CLI optional

--- vocata/cli/actor.py
+++ vocata/cli/actor.py
@@ -1,4 +1,5 @@
 from enum import StrEnum
+from typing import Optional
 
 import typer
 
@@ -21,13 +22,14 @@ def select_account(
     ctx: typer.Context,
     account: str = typer.Argument(..., help="Account name of new actor, in user@domain.tld format"),
 ):
+    # FIXME allow specifying account handle or URI
     ctx.obj["current_account"] = account
 
 
 @app.command()
 def create(
     ctx: typer.Context,
-    name: str = typer.Argument(..., help="Display name of new actor"),
+    name: Optional[str] = typer.Option(None, help="Display name of new actor"),
     actor_type: ActorType = typer.Option(ActorType.person, help="Actor type of new actor"),
     force: bool = typer.Option(
         False, help="Force creation even if prefix is not local (DANGEROUS!)"
@@ -37,6 +39,7 @@ def create(
     graph = get_graph(ctx.obj["settings"])
     account = ctx.obj["current_account"]
 
+    # FIXME allow specifying account handle or URI
     if not graph.is_valid_acct(account):
         ctx.obj["log"].error("The account name %s is invalid", account)
         raise typer.Exit(code=1)
@@ -45,7 +48,7 @@ def create(
         ctx.obj["log"].error("The account %s already exists", account)
         raise typer.Exit(code=1)
 
-    uri = graph.create_actor_from_acct(account, name, actor_type.value, force)
+    uri = graph.create_actor_from_acct(account, name or account, actor_type.value, force)
 
     if not uri:
         raise typer.Exit(code=2)
