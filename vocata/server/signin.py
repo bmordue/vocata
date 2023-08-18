c3744627937e2fd04beea257289bf2885b8a42af
Steve Ivy <steve@monkinetic.blog>
2023-08-18 08:11:05 -0700

    Add signin updates

--- vocata/server/signin.py
+++ vocata/server/signin.py
@@ -1,4 +1,5 @@
 import typing as t
+from urllib.parse import urlparse
 from starlette.endpoints import HTTPEndpoint
 
 # from starlette.requests import Request
@@ -6,6 +7,8 @@ from starlette.responses import RedirectResponse
 from starlette import status
 
 import logging
+
+from vocata.graph.authz import AccessMode
 
 if t.TYPE_CHECKING:
     from starlette.templating import TemplateResponse
@@ -15,13 +18,31 @@ logger = logging.getLogger(__name__)
 
 def handle_signin_error(request, err):
     return RedirectResponse(
-        request.url_for("signin") + f"?error={err}", status_code=status.HTTP_401_UNAUTHORIZED
+        str(request.url_for("signin")) + "?error={err}",
+        status_code=status.HTTP_401_UNAUTHORIZED,
     )
 
 
 class AuthSigninEndpoint(HTTPEndpoint):
+    def is_authenticated(self, request):
+        return hasattr(request.state, "actor") and request.state.actor
+
     async def get(self, request) -> "TemplateResponse":
-        # show login.html
+        # test if actor was already identified in the session
+        if self.is_authenticated(request):
+            uri = request.url_for("admin:dashboard")
+            # protect against being redirected off
+            # vocata
+            target = request.query_params.get("t")
+            if target:
+                potential_uri = urlparse(t)
+                with request.state.graph as graph:
+                    p = graph.get_url_prefix(potential_uri)
+                    if graph.is_local_prefix(p):
+                        uri = potential_uri
+
+            return RedirectResponse(uri)
+
         return request.state.templates.TemplateResponse(
             "login.html",
             {
@@ -33,40 +54,27 @@ class AuthSigninEndpoint(HTTPEndpoint):
     async def post(self, request) -> RedirectResponse:
         # process auth login
         logger.info("Signin request")
-
-        form = await request.form()
-        account = form.get("account")
-        password = form.get("password")
-
-        unauthed = handle_signin_error(
-            request,
-            "Could not authenticate user, please check your credentials "
-            "and try again, or contact the administrator.",
-        )
-
-        if not account and password:
-            return unauthed
-
-        with request.state.graph as graph:
-            actor = graph.get_canonical_uri(f"acct:{account}")
-            if not graph.verify_actor_password(actor, password):
-                return unauthed
-
-            # FIXME: use graph.is_authorized() but new
-            # AccessModes and/or actors needed
-            if not request.self.graph.get_actor_role(actor) == "admin":
-                return unauthed
-
-        return RedirectResponse(
-            request.url_for("admin.dashboard"),
-        )
+        if self.is_authenticated(request):
+            return request.state.templates.TemplateResponse(
+                "redirect.html",
+                {
+                    "request": request,
+                    "location": request.url_for("admin:dashboard"),
+                },
+            )
+        else:
+            handle_signin_error(
+                request,
+                "Could not authenticate user, please check your credentials "
+                "and try again, or contact the administrator.",
+            )
 
 
 class AuthSignoutEndpoint(HTTPEndpoint):
     async def get(self, request) -> "TemplateResponse":
         # clean session and return HTML direct
 
-        return request.state.templates.TemplateRespponse(
+        return request.state.templates.TemplateResponse(
             "redirect.html",
             {
                 "request": request,
